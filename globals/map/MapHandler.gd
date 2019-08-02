fae32d06c40d02fe0b2108ea08efa64a484ec8db
Zach777 <themanofthehour7778@gmail.com>
2019-08-02 17:37:03 -0400

    Made all methods Vector2 in MapHandler. Updated TurnTaker

--- globals/map/MapHandler.gd
+++ globals/map/MapHandler.gd
@@ -8,6 +8,8 @@ extends Node
 #1 for player, 
 #2 for enemy)
 
+const GROUND = 0
+
 var tiles : Array = []
 
 var tile_size : int = 32
@@ -28,9 +30,18 @@ func check_map( map : TileMap ) -> void :
 	pass
 
 
-func get_tile( x_position : int, y_position : int ) -> int :
-	return tiles[ x_position ][ y_position ]
+func get_tile( tile : Vector2 ) -> int :
+	return tiles[ tile.x ][ tile.y ]
 
 
-func set_tile( x_position : int, y_position : int, value : int ) -> void :
-	tiles[ x_position ][ y_position ] = value
+func move_unit( from_tiles : Vector2, to_tile : Vector2 ) -> void :
+	if tiles[ to_tile.x ][ to_tile.y ] != GROUND :
+		return
+	
+	var swap_value : int = tiles[ from_tiles.x ][ from_tiles.y ]
+	tiles[ from_tiles.x ][ from_tiles.y ] = 0
+	tiles[ to_tile.x ][ to_tile.y ] = swap_value
+
+
+func set_tile( tile : Vector2, value : int ) -> void :
+	tiles[ tile.x ][ tile.y ] = value
